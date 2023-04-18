b65e883556b39fa5f9ecfef82c79cfa203bb1552
Rand-Al <ezusod@gmail.com>
2023-04-18 14:14:16 +0300

    Finish upload image to publick folder

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -4,4 +4,9 @@ export default function handler(req, res) {
   if (req.method === "GET") {
     return res.status(200).json(inmemoryDb.getCourses());
   }
+  if (req.method === "PUT") {
+    const course = req.body;
+    inmemoryDb.editCourse(course);
+    return res.status(200).end();
+  }
 }
